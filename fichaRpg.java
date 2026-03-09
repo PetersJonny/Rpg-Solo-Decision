3c29a79ccb922618f6e6e6e1fb269fe0ec6531f0
PetersJonny <pedrojulio.barreto@gmail.comgit config --global user.name PetersJonnyagit config --global user.email pedrojulio.barreto@gmail.com>
2026-03-09 14:23:28 -0300

    Enviando commit

---
+++ fichaRpg.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class fichaRpg {
+	public static void main (String[] args) {
+		Scanner scn = new Scanner (System.in);
+	
+		String mago = "mago";
+		String guerreiro = "guerreiro";
+		String healer = "healer";
+	
+		String classeEscolhida = "";
+		
+		int classe = 0;
+
+		System.out.println("Escolha o nome do seu personagem:");
+		String nomePersonagem = scn.nextLine();
+		
+		System.out.println("Escolha o nome de quem é a ficha:");
+		String nomePessoa = scn.nextLine();
+
+		while(classe < 1 || classe > 3) {
+			System.out.println("\n Escolha entre uma das 3 classes abaixo: \n 1.Mago (só pode usar cajado, conjura magias poderosas, porém é mais fragil). \n 2.Guerreiro (só pode usar espada e atacar corpo a corpo, porém é mais resistente). \n 3.Healer (tem poderes de cura, pode curar a si mesmo e aos outros, tem uma vida mediana).");
+			classe = scn.nextInt();
+
+			switch(classe) {
+				case 1:
+					classeEscolhida = mago;
+					break;
+				case 2:
+					classeEscolhida = guerreiro;
+					break;
+				case 3: 
+					classeEscolhida = healer;
+					break;
+				default:
+					System.out.println("Escolha entre 1, 2 ou 3.");
+					
+			}
+		
+		}
+
+		System.out.println("\n --------FICHA-------- \n\nNome: " + nomePersonagem + "\t\tDono da ficha: " + nomePessoa + "\t\tClasse: " + classeEscolhida + "\n----------------------");
+
+	}
+}
