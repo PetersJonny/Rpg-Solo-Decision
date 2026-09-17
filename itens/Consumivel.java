ae80bdecee5ba97703e0904cbe400da530f17fc9
Marcus Martins <vini.mark2002@gmail.com>
2026-09-17 16:57:03 -0300

    build: Migração para Maven e Java 21
    Deleta os arquivos da raiz e adiciona estrutura de build moderna.

--- itens/Consumivel.java
+++
@@ -1,7 +0,0 @@
-package itens;
-
-public class Consumivel extends ItemRpg {
-    public Consumivel(String nome, String descricao, int quantidade) {
-        super(nome, descricao, quantidade);
-    }
-}
